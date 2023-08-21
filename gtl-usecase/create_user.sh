19d4865553e87bdaf56205e00fdaa1a4d9d69b0a
tiventiven <tiven.tiven@gtl.id>
2023-08-21 22:50:02 +0700

    update grand_access dan create_user

--- gtl-usecase/create_user.sh
+++ gtl-usecase/create_user.sh
@@ -1,15 +1,42 @@
 # Replace these values with your actual information
-ipss=$ipss
-userdb=$userdb
-dbname=$dbname
+ipps=192.168.64.8
+userdb=admin
+dbname=logical
     
+    echo "++++++++++++++++"
+    echo "Checking db user"
+    echo "++++++++++++++++"
+    echo ""
+    sleep 3
     validate=`psql -X -h $ipps -U $userdb -d $dbname -t -c "SELECT count(*) FROM pg_roles where rolname = 'gtl_bqetl_stg';" | xargs`
     validatedb=`psql -X -h $ipps -U $userdb -d $dbname -t -c "SELECT count(*) FROM pg_database where datname = '$dbname';" | xargs`
 
  if [[ $validate = 0 && $validatedb = 1 ]]
     then
         psql -X -h $ipps -U $userdb  -d $dbname -c "set role = gtldb_superuser;" -c "CREATE ROLE gtl_bqetl_stg;"
-        psql -X -h $ipps -U $userdb  -d $dbname -c "set role = gtldb_superuser;" -c "ALTER ROLE gtl_bqetl_stg PASSWORD '';"
-        psql -X -h $ipps -U $userdb  -d $dbname -c "set role = gtdb_superuser;" -c "COMMENT ON ROLE gtl_bqetl_stg  IS 'blablabla';"
-        psql -X -h $ipps -U $userdb  -d $dbname -c "set role = gtdb_superuser;" -c "ALTER GROUP analytic_access ADD USER gtl_bqetl_stg;"
+        psql -X -h $ipps -U $userdb  -d $dbname -c "set role = gtldb_superuser;" -c "ALTER ROLE gtl_bqetl_stg PASSWORD '123';"
+        psql -X -h $ipps -U $userdb  -d $dbname -c "set role = gtldb_superuser;" -c "COMMENT ON ROLE gtl_bqetl_stg  IS 'blablabla';"
+        echo "Create role gtl_bqetl_stg done";
+        sleep 2
 fi
+    echo "+++++++++++++++++"
+    echo "Checking db group"
+    echo "+++++++++++++++++"
+    echo ""
+    sleep 3
+    validategroup=`psql -X -h $ipps -U $userdb -d $dbname -t -c "\du+ gtl_bqetl_stg"|grep -wc analytic_access|xargs`
+            if [ "$validategroup" = 0 ]
+       then
+        psql -X -h $ipps -U $userdb  -d $dbname -c "set role = gtldb_superuser;" -c "GRANT analytic_access to gtl_bqetl_stg;"
+fi
+valcreate=`psql -X -h $ipps -U $userdb -d $dbname -t -c "SELECT count(*) FROM pg_roles where rolname = 'gtl_bqetl_stg';" | xargs`
+
+    if [ $valcreate = 1 ]
+    then
+        echo "bqetl_stg is already registered on this DB";
+	echo ""
+	sleep 2
+    else
+        echo "failed to create role bqetl_stg please contact DBA team";
+	sleep 2
+    fi
--- gtl-usecase/grant_access.sh
+++ gtl-usecase/grant_access.sh
@@ -1,27 +1,75 @@
 # Replace these values with your actual information
-ipss=$ipss
-userdb=$userdb
-dbname=$dbname
-    
-    validate=`psql -X -h $ipps -U $userdb -d $dbname -t -c "SELECT count(*) FROM pg_roles where rolname = 'gtl_bqetl_stg';" | xargs`
+ipps=192.168.64.8
+userdb=admin
+dbname=logical
+tablename=logical_test
+
+ validate=`psql -X -h $ipps -U $userdb -d $dbname -t -c "SELECT count(*) FROM pg_roles where rolname = 'gtl_bqetl_stg';" | xargs`
     validatedb=`psql -X -h $ipps -U $userdb -d $dbname -t -c "SELECT count(*) FROM pg_database where datname = '$dbname';" | xargs`
 
-if [[ $validate = 1 && $validatedb = 1 ]]
-    then
-IFS=','
+ if [[ $validate = 0 && $validatedb = 1 ]]
+ then
+    echo "+++++++++++++++++++++++++++"
+    echo "Checking all good, continue"
+    echo "+++++++++++++++++++++++++++"
+    echo ""
+    sleep 3
+
+else
+    echo "Validation failed. Either gtl_bqetl_stg role is missing or the database $dbname doesn't exist."
+    exit 1  # This will stop the script
+fi
+
+  echo "+++++++++++++++++++++++++++"
+    echo "Checking table availability"
+    echo "+++++++++++++++++++++++++++"
+    echo ""
+    sleep 3
+    tablecount=0
+
+  IFS=','
     for table in $tablename
     do
-        psql -X -h $ipps -U $userdb -d $dbname -c "\z $table"
-        psql -X -h $ipps -U $userdb -d $dbname -c "grant select on $table to analytic_access;"
-        psql -X -h $ipps -U $userdb -d $dbname -c "\z $table"
-
-	validatetables=`psql -X -h $ipps -U $userdb -d $dbname -t -c "\z $table"|grep -wc analytic_access|xargs`
-    	if [ "$validatetables" = 1 ]
-
-        then
-            echo "Grant select for table $table Done";
+    tablecount=$tablecount
+    validatetables=`psql -X -h $ipps -U $userdb -d $dbname -t -c "\dt"|grep -wc $table|xargs`
+    if [ "$validatetables" = 0 ]
+    then
+        echo "Table $table doesn't exists";
+	    tablecount=$((tablecount + 1))
 	    sleep 3
-        else
-            echo "Grant select for table $table Failed";
-	    sleep 3
-        fi
+    fi
+    done
+
+    if [ $tablecount = 0 ]
+    then
+        echo "OK.. Table exists, continue to next step"
+	echo ""
+    else
+        echo "Please recheck table name, make sure table exists on database $dbname"
+	echo ""
+        exit 1
+    fi
+    sleep 5
+
+    echo ""
+    echo "++++++++++++++++++++++++++++++++++++++++++"
+    echo "Grant Select on listed tables to bqetl_stg"
+    echo "++++++++++++++++++++++++++++++++++++++++++"
+    echo ""
+    sleep 3
+
+IFS=','
+for table in $tablename; do
+    psql -X -h $ipps -U $userdb -d $dbname -c "\z $table"
+    psql -X -h $ipps -U $userdb -d $dbname -c "grant select on $table to analytic_access;"
+    psql -X -h $ipps -U $userdb -d $dbname -c "\z $table"
+
+    validatetables=$(psql -X -h $ipps -U $userdb -d $dbname -t -c "\z $table" | grep -wc admin | xargs)
+    if [ "$validatetables" = 1 ]; then
+        echo "Grant select for table $table Done"
+        sleep 3
+    else
+        echo "Grant select for table $table Failed"
+        sleep 3
+    fi
+done
