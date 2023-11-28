96655f19cbe531b32178f169737189182ff75da2
tiventiven <tiven.tiven@gtl.id>
2023-11-28 14:23:31 +0700

    add2

--- script_sql/timeout_granting.sql
+++ script_sql/timeout_granting.sql
@@ -1,4 +1,4 @@
-alter role avnhasura set statement_timeout to '15s';
+alter role avnhasura set statement_timeout to '60s';
 alter role avnhasura set idle_in_transaction_session_timeout to '30s';
 alter role avnhasura set idle_session_timeout to '0s';
 
@@ -6,24 +6,24 @@ alter role hasuraapiread set statement_timeout to '60s';
 alter role hasuraapiread set idle_in_transaction_session_timeout to '30s';
 alter role hasuraapiread set idle_session_timeout to '0s';
 
-alter role avninbound set statement_timeout to '15s';
-alter role avninbound set idle_in_transaction_session_timeout to '15s';
+alter role avninbound set statement_timeout to '60s';
+alter role avninbound set idle_in_transaction_session_timeout to '30s';
 alter role avninbound set idle_session_timeout to '0s';
 
 alter role "avninbound-evco" set statement_timeout to '15s';
-alter role "avninbound-evco" set idle_in_transaction_session_timeout to '15s';
+alter role "avninbound-evco" set idle_in_transaction_session_timeout to '30s';
 alter role "avninbound-evco" set idle_session_timeout to '0s';
 
 alter role wmsinboundv3 set statement_timeout to '15s';
-alter role wmsinboundv3 set idle_in_transaction_session_timeout to '15s';
+alter role wmsinboundv3 set idle_in_transaction_session_timeout to '30s';
 alter role wmsinboundv3 set idle_session_timeout to '0s';
 
 alter role wmsinboundv3evco set statement_timeout to '15s';
-alter role wmsinboundv3evco set idle_in_transaction_session_timeout to '15s';
+alter role wmsinboundv3evco set idle_in_transaction_session_timeout to '30s';
 alter role wmsinboundv3evco set idle_session_timeout to '0s';
 
-alter role avnetl set statement_timeout to '30s';
-alter role avnetl set idle_in_transaction_session_timeout to '30s';
+alter role avnetl set statement_timeout to '30m';
+alter role avnetl set idle_in_transaction_session_timeout to '30m';
 alter role avnetl set idle_session_timeout to '0s';
 
 alter role avnshipment set statement_timeout to '60s';
